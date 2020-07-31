43c3f31f0d1ce0dad0bc347662d4ded073024363
vpk-test <68972921+vpk-test@users.noreply.github.com>
2020-07-31 19:25:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 const oneplusone = () => 2;
-const oneplustwo = () => 3;
-const oneplusthree = () => 4;
-const oneplusfour = () => 5;
-const oneplus5 = () => 6;
-const oneplus6 = () => 7;
-const oneplus7 = () => 8;
-const oneplus8 = () => 9;
-const oneplus9 = () => 10;
-const oneplus10 = () => 11;
-const oneplus11 = () => 12;
-const oneplus12 = () => 13;
+const logNumbers = () => console.log([]);
 
 module.exports = oneplusone;
